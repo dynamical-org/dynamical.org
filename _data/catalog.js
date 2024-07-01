9ecf8a3722f21cc50ce7a3ddb886fc8c0291875c
mrshll <m@mrshll.com>
2024-06-30 21:23:30 -0400

    add catalog

---
+++ _data/catalog.js
@@ -0,0 +1,40 @@
+const fetch = require('@11ty/eleventy-fetch');
+
+let catalog = [
+  {
+    name: 'NOAA GFS 6-hourly Analysis',
+    description:
+      "Historical weather data from the US National Oceanic and Atmospheric Administration's Global Forecast System",
+    url: 'https://data.dynamical.org/gfs/analysis/latest.zarr',
+		status: 'todo',
+  },
+];
+
+module.exports = async function () {
+  for (let i = 0; i < catalog.length; i++) {
+    const metadata = (await fetch(`${catalog[i].url}/.zmetadata`, {type: 'json'}))['metadata'];
+    const metadataKeys = Object.keys(metadata);
+
+    let dimensions = [];
+    let variables = [];
+    for (let i = 0; i < metadataKeys.length; i++) {
+      const key = metadataKeys[i];
+      if (key.endsWith('.zattrs') && metadata[key]['_ARRAY_DIMENSIONS']) {
+        const name = key.substring(0, key.indexOf('/'));
+        if (metadata[key]['_ARRAY_DIMENSIONS'].length === 1) {
+          dimensions.push({
+            name,
+            ...metadata[key],
+            ...metadata[`${name}/.zarray`],
+          });
+        } else {
+          variables.push({name, ...metadata[key], ...metadata[`${name}/.zarray`]});
+        }
+      }
+    }
+    catalog[i].dimensions = dimensions;
+    catalog[i].variables = variables;
+  }
+
+  return catalog;
+};
