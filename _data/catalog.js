453e3f57befe39410c083f4e3f9c9600869a315d
mrshll <m@mrshll.com>
2024-07-01 12:55:03 -0400

    Future proof dimension logic in catalog generation

--- _data/catalog.js
+++ _data/catalog.js
@@ -1,4 +1,5 @@
 const fetch = require('@11ty/eleventy-fetch');
+const {uniq} = require('lodash');
 
 let catalog = [
   {
@@ -8,41 +9,40 @@ let catalog = [
     url: 'https://data.dynamical.org/gfs/analysis/latest.zarr',
     status: 'Initial release',
   },
-  {
-    name: 'NOAA GFS 6-hourly Analysis',
-    description:
-      "Historical weather data from the US National Oceanic and Atmospheric Administration's Global Forecast System",
-    url: 'https://data.dynamical.org/gfs/analysis/latest.zarr',
-    status: 'Phase 1 roadmap',
-  },
 ];
 
 module.exports = async function () {
   for (let i = 0; i < catalog.length; i++) {
-    let metadataKeys = {};
-    let metadata = {};
-    if (catalog[i].url) {
-      metadata = (await fetch(`${catalog[i].url}/.zmetadata`, {type: 'json'}))['metadata'];
-      metadataKeys = Object.keys(metadata);
+    if (!catalog[i].url) {
+      continue;
     }
+
+    const metadata = (await fetch(`${catalog[i].url}/.zmetadata`, {type: 'json'}))['metadata'];
+    const metadataKeys = Object.keys(metadata);
+    const dimensionKeys = uniq(
+      metadataKeys.flatMap((key) => metadata[key]['_ARRAY_DIMENSIONS']).filter((key) => !!key)
+    );
+    const variableKeys = metadataKeys
+      .filter((key) => key.endsWith('.zattrs') && metadata[key]['_ARRAY_DIMENSIONS'])
+      .map((key) => key.substring(0, key.indexOf('/')))
+      .filter((key) => !dimensionKeys.includes(key));
 
     let dimensions = [];
     let variables = [];
-    for (let i = 0; i < metadataKeys.length; i++) {
-      const key = metadataKeys[i];
-      if (key.endsWith('.zattrs') && metadata[key]['_ARRAY_DIMENSIONS']) {
-        const name = key.substring(0, key.indexOf('/'));
-        if (metadata[key]['_ARRAY_DIMENSIONS'].length === 1) {
-          dimensions.push({
-            name,
-            ...metadata[key],
-            ...metadata[`${name}/.zarray`],
-          });
-        } else {
-          variables.push({name, ...metadata[key], ...metadata[`${name}/.zarray`]});
-        }
-      }
+    for (let i = 0; i < dimensionKeys.length; i++) {
+      const key = dimensionKeys[i];
+      dimensions.push({
+        name: key,
+        ...metadata[`${key}/.zattrs`],
+        ...metadata[`${key}/.zarray`],
+      });
     }
+
+    for (let i = 0; i < variableKeys.length; i++) {
+      const key = variableKeys[i];
+      variables.push({name: key, ...metadata[`${key}/.zattrs`], ...metadata[`${key}/.zarray`]});
+    }
+
     catalog[i].dimensions = dimensions;
     catalog[i].variables = variables;
   }
