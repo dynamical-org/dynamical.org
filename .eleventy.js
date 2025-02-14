04a18d1a1e0e1de6134dcb481d1856c3d64aa33c
mrsll <m@mrshll.com>
2025-02-14 10:06:36 -0600

    +

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const pluginRss = require('@11ty/eleventy-plugin-rss');
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const {pairedShortcode} = require('@11ty/eleventy-plugin-syntaxhighlight');
 const {EleventyHtmlBasePlugin} = require('@11ty/eleventy');
+const fetch = require('node-fetch');
 
 const pluginImages = require('./eleventy.config.images.js');
 
@@ -28,6 +29,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('readableDate', (dateObj, format, zone) => {
     // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
     return DateTime.fromJSDate(dateObj, {zone: zone || 'utc'}).toFormat(format || 'yyyy-MM-dd');
+  });
+
+  eleventyConfig.addGlobalData("contributors", async () => {
+    const response = await fetch('https://api.github.com/orgs/dynamical-org/repos');
+    const repos = await response.json();
+    const contributorsSet = new Set();
+
+    for (const repo of repos) {
+      const contributorsResponse = await fetch(repo.contributors_url);
+      const contributors = await contributorsResponse.json();
+      contributors.forEach(contributor => contributorsSet.add(contributor.login));
+    }
+
+    return Array.from(contributorsSet).sort();
   });
 
   return {
