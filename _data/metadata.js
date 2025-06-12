59c1789fa65d86ded860f3d5ed0326e1c599d895
mrsll <m@mrshll.com>
2025-06-11 21:22:44 -0500

    +

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-  title: 'dynamical.org',
-  url: 'https://dynamical.org/',
-  language: 'en',
+  title: "dynamical.org",
+  url: "https://dynamical.org/",
+  language: "en",
   description:
     "Advancing humanity's ability to access, understand, and act on accurate weather and climate data.",
   author: {
-    name: 'dynamical.org',
-    email: 'marshall@dynamical.org',
+    name: "dynamical.org",
+    email: "marshall@dynamical.org",
   },
 };
